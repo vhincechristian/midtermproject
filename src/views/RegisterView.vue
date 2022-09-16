bb8b1797ef819af2f7754f209dd9e2e72ceaa9d1
vhincechristian <mvcdcugot@tip.edu.ph>
2022-09-16 15:48:37 +0800

    asd

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -3,5 +3,44 @@
     <p><input type="text" playceholder="Email" v-model="email"/></p>
     <p><input type="password" playceholder="Password" v-model="password"/></p>
     <p><button @click="register">Submit</button></p>
-    <p><button @click="signInWithGoogle"></button></p>
+    <p><button @click="signInWithGoogle">Sign In With Google</button></p>
 </template>
+
+<script setup>
+    import { provide, ref } from "vue";
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { useRouter } from "vue-router";
+import router from "../router";
+
+const email = ref("");
+const password = ref("");
+const auth = getAuth();
+const register = () => {
+  createUserWithEmailAndPassword(getAuth(), email.value, password.value)
+    .then((data) => {
+      console.log("Successfully registered!");
+      console.log(auth.currentUser);
+      router.push("/");
+    })
+    .catch((error) => {
+      console.log(error.code);
+      alert(error.message);
+    });
+};
+
+const signInWithGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(getAuth(), provider)
+    .then((result) => {
+      console.log(result.user);
+      router.push("/");
+    })
+    .catch((error) => {});
+};
+</script>
