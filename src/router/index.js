0d594d2069839ed794f7f81226c35ba03fdfbeed
vhincechristian <mvcdcugot@tip.edu.ph>
2022-09-18 17:44:10 +0800

    adasd

--- src/router/index.js
+++ src/router/index.js
@@ -99,6 +99,15 @@ const router = createRouter({
             },
         },
 
+        {
+            path: '/QuizHistoryView',
+            name: 'QuizHistory',
+            component: QuizHistoryView,
+            meta: {
+                requiresAuth: true,
+            },
+        },
+
     ]
 })
 
